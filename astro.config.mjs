ce4194a5702dc06216665d7df1c9298adb506abd
diego <diegogj17@hotmail.es>
2026-03-23 21:52:49 +0100

    feat: sliders con flechas, portfolio, SEO, favicon, fondos verdes difuminados

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aljesolutions.com',
+  devToolbar: {
+    enabled: false,
+  },
   integrations: [
     sitemap({
       filter: (page) =>
